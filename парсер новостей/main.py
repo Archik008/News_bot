8c232736343847ee2619ac5f114d8a3d9131c124
Артем <119063410+Archik008@users.noreply.github.com>
2023-06-22 18:12:01 +0600

    Update main.py

--- парсер новостей/main.py
+++ парсер новостей/main.py
@@ -1,14 +1,48 @@
-from bs4 import BeautifulSoup
+import json
+import random
 
-with open('page.html', 'r', encoding='utf-8') as file:
-    page = file.read()
+from aiogram import Bot, Dispatcher, types, executor
 
-soup = BeautifulSoup(page, 'lxml')
 
-soup.find_parent()
+async def on_start(_):
+    print('Бот запустился')
 
-same_blocks = soup.find_all(class_='uk-overlay-primary')
-# for block in same_blocks:
-    
 
-# print(page)
+async def on_shutdown(_):
+    print('Бот остановился')
+
+
+#
+with open('data.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+news_descriptions = list(data.keys())
+
+API_TOKEN = '6287021376:AAElQ4_lTq5o7Had1Gdbg0hjlMeN-g4tV-A'
+
+bot = Bot(API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def say_hello(msg: types.Message):
+    await msg.answer('Hi! Type /send_news to get the news.')
+
+
+@dp.message_handler(commands=['send_news'])
+async def sending_news(msg: types.Message):
+    chosen_news = random.choice(news_descriptions)
+    value_of_key = data[chosen_news]
+    if isinstance(value_of_key, str):
+        msg_random_news = f'{chosen_news}\n{value_of_key}'
+        await msg.reply(msg_random_news)
+    elif isinstance(value_of_key, dict):
+        href = value_of_key['Ссылка']
+        time = value_of_key['Время']
+        hashtag = value_of_key['Хэштэг'] if 'Хэштэг' in value_of_key.keys() else ''
+        send_news = f'{chosen_news}\n{href} - {time}\n{hashtag}'
+        await msg.reply(send_news)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_start, on_shutdown=on_shutdown)
